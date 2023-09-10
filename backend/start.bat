885f07a54f506a7488fd98864b53cf0663f0afff
artontech <artontech@163.com>
2023-09-10 13:19:43 +0800

    update start script

--- backend/start.bat
+++ backend/start.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 cd /d "%~dp0"
-SET PATH=.\runtime\python;%PATH%
+SET CUDA_BIN_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.1\bin
+SET PATH=.\runtime\python;%CUDA_BIN_PATH%;%PATH%
 python %~dp0server.py
 pause
--- frontend/start.bat
+++ frontend/start.bat
@@ -1,2 +1,5 @@
+@ECHO OFF
+cd %~dp0
+SET NODE_OPTIONS=--openssl-legacy-provider
 yarn run serve
 pause
